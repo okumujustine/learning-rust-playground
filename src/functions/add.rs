1b51f82851b6947d47299298f7d7b46f135b0a70
Justine Okumu <okumujustine01@gmail.com>
2025-01-02 22:12:18 -0600

    adding test cases

--- src/functions/add.rs
+++ src/functions/add.rs
@@ -3,5 +3,20 @@ pub fn add_five(num: u32) -> u32 {
 }
 
 pub fn add_10(num: u8) -> u8 {
-    num - 10
+    num + 10
+}
+
+#[cfg(test)]
+mod test {
+    use crate::functions::add::{add_10, add_five};
+
+    #[test]
+    fn test_add_five() {
+        assert_eq!(add_five(50), 55);
+    }
+
+    #[test]
+    fn test_add_10 () {
+        assert_eq!(add_10(11), 21);
+    }
 }
--- src/functions/minus.rs
+++ src/functions/minus.rs
@@ -1,3 +1,13 @@
 pub fn subtract_10(num: u8) -> u8 {
     num - 10
 }
+
+#[cfg(test)]
+mod test {
+    use crate::functions::minus::subtract_10;
+
+    #[test]
+    fn test_subtract_10() {
+        assert_eq!(subtract_10(10), 0);
+    }
+}
