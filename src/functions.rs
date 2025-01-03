b5e054a92f5a439790abf8eb284b3fafd8c8c325
Justine Okumu <okumujustine01@gmail.com>
2025-01-02 21:55:42 -0600

    importing modules in rust

---
+++ src/functions.rs
@@ -0,0 +1,7 @@
+pub fn add_five(num: u32) -> u32 {
+    num + 5
+}
+
+pub fn subtract_10(num: u8) -> u8 {
+    num - 10
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+mod functions;
 
-fn add_five(num: u32) -> u32 {
+use crate::functions::{ add_five, subtract_10 };
 
-}
 // everything in rust is immutable by default
 fn main() {
+    add_five(5);
+    subtract_10(16);
+
+    let mut x: i32 = 33;
+    println!("{x}");
+    x = 50;
+    println!("{x}");
 }
